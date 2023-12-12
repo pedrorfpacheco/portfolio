7e0b7e1b6877f0a3b509b93d46f7f44f91c5b66a
Pedro Pacheco <148588945+pedrorfpacheco@users.noreply.github.com>
2023-12-12 17:33:10 +0000

    adding the mechanism of mobile behaviour

--- portfolio_react/src/components/HeaderBar/HeaderBarStyledComponents.js
+++ portfolio_react/src/components/HeaderBar/HeaderBarStyledComponents.js
@@ -3,7 +3,7 @@ import {Link as LinkR} from "react-router-dom";
 import {darkTheme} from "../../utils/Themes";
 
 export const Nav = styled.div`
-  background-color: ${({ theme }) => theme.cardLight};
+  background-color: ${({theme}) => theme.cardLight};
   height: 80px;
   display: flex;
   justify-content: center;
@@ -35,22 +35,8 @@ export const NavLogo = styled(LinkR)`
   cursor: pointer;
   text-decoration: none;
   align-items: center;
-  @media screen and (max-width: 640px) {
-    padding: 0 0;
-  }
-`;
-
-export const MobileIcon = styled.div`
-  display: none;
-  @media screen and (max-width: 768px) {
-    display: block;
-    position: absolute;
-    top: 0;
-    right: 0;
-    transform: translate(-100%, 50%);
-    font-size: 1.5rem;
-    cursor: pointer;
-    color: ${() => darkTheme.textPrimary};
+  @media screen and (max-width: 380px) {
+    display: none;
   }
 `;
 
@@ -61,20 +47,20 @@ export const NavItems = styled.ul`
   align-items: center;
   gap: 32px;
   list-style: none;
-  @media screen and (max-width: 768px) {
+  @media screen and (max-width: 900px) {
     display: none;
   }
 `;
 
 export const NavLink = styled.a`
-  color: ${({ theme }) => theme.textPrimary};
+  color: ${({theme}) => theme.textPrimary};
   font-weight: 500;
   cursor: pointer;
   text-decoration: none;
   transition: all 0.2s ease-in-out;
 
   &:hover {
-    color: ${({ theme }) => theme.primary};
+    color: ${({theme}) => theme.primary};
   }
 `;
 
@@ -85,14 +71,14 @@ export const ButtonContainer = styled.div`
   width: 80%;
   height: 100%;
   padding: 0 6px;
-  @media screen and (max-width: 640px) {
+  @media screen and (max-width: 768px) {
     display: none;
   }
 `;
 
 export const SocialMediaIcon = styled.a`
   background-color: transparent;
-  color: ${({ theme }) => theme.textPrimary};
+  color: ${({theme}) => theme.textPrimary};
   padding: 0 0;
   display: flex;
   justify-content: center;
@@ -105,6 +91,55 @@ export const SocialMediaIcon = styled.a`
   margin-right: 30px;
 
   :hover {
-    color: ${({ theme }) => theme.primary};
+    color: ${({theme}) => theme.primary};
+  }
+`;
+
+export const MobileIcon = styled.div`
+  display: none;
+  @media screen and (max-width: 768px) {
+    display: block;
+    position: absolute;
+    top: -1px;
+    right: 10px;
+    transform: translate(-100%, 50%);
+    font-size: 2rem;
+    cursor: pointer;
+    color: ${() => darkTheme.textPrimary};
+  }
+`;
+
+export const MobileMenu = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 16px;
+  position: absolute;
+  top: 80px;
+  right: 0;
+  width: 100%;
+  padding: 12px 40px 24px 40px;
+  background: ${({theme}) => theme.cardLight};
+  transition: all 0.6s ease-in-out;
+  transform: ${({isOpen}) => (isOpen ? 'translateY(0)' : 'translateY(-100%)')};
+  border-radius: 0 0 20px 20px;
+  box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.2);
+  opacity: ${({isOpen}) => (isOpen ? '98%' : '0')};
+  z-index: ${({isOpen}) => (isOpen ? '1000' : '-1000')};
+`
+
+export  const MobileLink = styled.a`
+  color: ${({theme}) => theme.textPrimary};
+  font-weight: 500;
+  cursor: pointer;
+  transition: all 0.2s ease-in-out;
+  text-decoration: none;
+
+  :hover {
+    color: ${({theme}) => theme.primary};
+  }
+
+  &.active {
+    border-bottom: 2px solid ${({theme}) => theme.primary};
   }
 `;
--- portfolio_react/src/components/HeaderBar/index.js
+++ portfolio_react/src/components/HeaderBar/index.js
@@ -1,6 +1,6 @@
 import React, {useContext} from "react";
 import {
-    ButtonContainer,
+    ButtonContainer, MobileIcon, MobileLink, MobileMenu,
     Nav,
     NavContainer,
     NavItems,
@@ -17,8 +17,10 @@ import DarkModeIcon from '@mui/icons-material/DarkMode';
 import LightModeIcon from '@mui/icons-material/LightMode';
 import {darkTheme} from "../../utils/Themes";
 import {ThemeContext} from "styled-components";
+import {FaBars} from "react-icons/fa";
 
 const HeaderBar = ({onThemeToggle}) => {
+    const [isOpen, setIsOpen] = React.useState(false);
     const theme = useContext(ThemeContext);
 
     return (
@@ -29,6 +31,11 @@ const HeaderBar = ({onThemeToggle}) => {
                         <img src={LogoPedroPacheco} alt='hero-image' style={{height: '35px', width: '270px'}}/> :
                         <img src={LogoPedroPachecoWhite} alt='hero-image' style={{height: '35px', width: '270px'}}/>}
                 </NavLogo>
+                <MobileIcon>
+                    <FaBars onClick={() => {
+                        setIsOpen(!isOpen)
+                    }} />
+                </MobileIcon>
                 <NavItems>
                     <NavLink href="#about">About</NavLink>
                     <NavLink href="#experience">Experience</NavLink>
@@ -48,6 +55,34 @@ const HeaderBar = ({onThemeToggle}) => {
                         <GitHubIcon style={{width: '37px', height: '37px'}}/>
                     </SocialMediaIcon>
                 </ButtonContainer>
+                {
+                    isOpen &&
+                    <MobileMenu isOpen={isOpen}>
+                        <MobileLink href="#about" onClick={() => {
+                            setIsOpen(!isOpen)
+                        }}>About</MobileLink>
+                        <MobileLink href='#experience' onClick={() => {
+                            setIsOpen(!isOpen)
+                        }}>Experience</MobileLink>
+                        <MobileLink href='#education' onClick={() => {
+                            setIsOpen(!isOpen)
+                        }}>Education</MobileLink>
+                        <MobileLink href='#contacts' onClick={() => {
+                            setIsOpen(!isOpen)
+                        }}>Contacts</MobileLink>
+                        <MobileLink href={Bio.linkedin} onClick={() => {
+                            setIsOpen(!isOpen)
+                        }}>LinkedIn</MobileLink>
+                        <MobileLink href={Bio.github} onClick={() => {
+                            setIsOpen(!isOpen)
+                        }}>Github</MobileLink>
+                        <SocialMediaIcon onClick={onThemeToggle}>
+                            {theme === darkTheme ?
+                                <LightModeIcon style={{width: '38px', height: '38px'}}/> :
+                                <DarkModeIcon style={{width: '46px', height: '46px'}}/>}
+                        </SocialMediaIcon>
+                    </MobileMenu>
+                }
             </NavContainer>
         </Nav>
     );
